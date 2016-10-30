eb9e31ff8120ea2cdcfc0a1f153ce6a9f908c79c
Denylson Melo <denylsonmelo@gmail.com>
2016-10-29 23:46:38 -0300

    modificação no album de foto

--- js/fadein-talk.js
+++ js/fadein-talk.js
@@ -1,3 +1,14 @@
+// SCRIPT DE EFEITO FADE-IN BY "JOÃO PAULO CARVALHO"
 function fadein(domElement) {
 	document.getElementById(domElement).style.opacity = 1;
+}
+function fadeH2(type) {
+	if ( type == "in" ) {
+		document.getElementById("fadeinH2").style.color = 'white';
+		document.getElementById("fadeinH2").style.background = '#23532f';
+	}
+	else if ( type == "out" ) {
+		document.getElementById("fadeinH2").style.color = 'black';
+		document.getElementById("fadeinH2").style.background = '#b9f9b1';
+	}
 }
