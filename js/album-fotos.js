50230fd696b242601e0b513832697038b82dda38
JJPaulo2 <jjpaulo2@hotmail.com>
2016-11-13 02:50:49 -0300

    corrigindo bug de imagens não carregarem e melhorias no album de fotos e nos links do site

--- js/album-fotos.js
+++ js/album-fotos.js
@@ -1,43 +1,58 @@
 // SCRIPT DE ALBUM DE FOTOS BY "JOÃO PAULO CARVALHO"
 function photo(numPhoto) {
-	if ( numPhoto==1 ) {
-		document.getElementById("img").src="http://i64.tinypic.com/2yphhja.jpg";
+	if ( numPhoto==0 ) {
+		document.getElementById("img").src="img/fotos-snct/reufi00.jpg";
+		document.getElementById("description").innerHTML="Professores Denylson Melo e Marcia Regina juntos á alguns alunos da turma." ;
+		document.getElementById("viewClass").style.margin = "0px 0px 0px -5000px";
+	}
+	else if ( numPhoto==1 ) {
+		document.getElementById("img").src="img/fotos-snct/reufi01.jpg";
 		document.getElementById("description").innerHTML="Alunos André Henrique e João Paulo apresentado o site." ;
+		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
 	}
 	else if ( numPhoto==2 ) {
-		document.getElementById("img").src="http://i68.tinypic.com/180zmp.jpg";
+		document.getElementById("img").src="img/fotos-snct/reufi02.jpg";
 		document.getElementById("description").innerHTML="Aluno Manoel Eric falando a respeito do projeto." ;
+		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
 	}
 	else if ( numPhoto==3 ) {
-		document.getElementById("img").src="http://i64.tinypic.com/2a4oft4.jpg";
+		document.getElementById("img").src="img/fotos-snct/reufi03.jpg";
 		document.getElementById("description").innerHTML="Aluno João Paulo apresentando o projeto." ;
+		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
 	}
 	else if ( numPhoto==4 ) {
-		document.getElementById("img").src="http://i68.tinypic.com/1z5ntvk.jpg";
+		document.getElementById("img").src="img/fotos-snct/reufi04.jpg";
 		document.getElementById("description").innerHTML="Aluno André Henrique falando a respeito do site." ;
+		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
 	}
 	else if ( numPhoto==5 ) {
-		document.getElementById("img").src="http://i67.tinypic.com/157bu51.jpg";
+		document.getElementById("img").src="img/fotos-snct/reufi05.jpg";
 		document.getElementById("description").innerHTML="Alunos André Henrique e João Paulo apresentando o projeto." ;
+		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
 	}
 	else if ( numPhoto==6 ) {
-		document.getElementById("img").src="http://i67.tinypic.com/35i9g5t.jpg";
+		document.getElementById("img").src="img/fotos-snct/reufi06.jpg";
 		document.getElementById("description").innerHTML="Alunos de Nutrição assistindo a palestra sobre aplicativos que já nos ajudam a manter uma vida saudável." ;
+		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
 	}
 	else if ( numPhoto==7 ) {
-		document.getElementById("img").src="http://i65.tinypic.com/2zof4nn.jpg";
+		document.getElementById("img").src="img/fotos-snct/reufi07.jpg";
 		document.getElementById("description").innerHTML="Alunos Manoel Eric e André Henrique apresentando o site." ;
+		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
 	}
 	else if ( numPhoto==8 ) {
-		document.getElementById("img").src="http://i64.tinypic.com/2usdgf8.jpg";
+		document.getElementById("img").src="img/fotos-snct/reufi08.jpg";
 		document.getElementById("description").innerHTML="Aluno João Paulo apresentando o site." ;
+		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
 	}
 	else if ( numPhoto==9 ) {
-		document.getElementById("img").src="http://i67.tinypic.com/25a2r20.jpg.jpg";
+		document.getElementById("img").src="img/fotos-snct/reufi09.jpg";
 		document.getElementById("description").innerHTML="Aluna Stefany Brazil e aluno Tawan Rodrigues explicando as vantagens de se ingerir suco de manga." ;
+		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
 	}
 	else if ( numPhoto==10 ) {
-		document.getElementById("img").src="http://i64.tinypic.com/2vb7cbc.jpg.jpg";
+		document.getElementById("img").src="img/fotos-snct/reufi10.jpg";
 		document.getElementById("description").innerHTML="Aluno Manoel Eric apresentando o site." ;
+		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
 	}
 }
