8fe946766948a5824601f3f924c36739f2bb01a6
JJPaulo2 <jjpaulo2@hotmail.com>
2016-11-14 03:26:49 -0300

    primeira interface mobile

--- js/album-fotos.js
+++ js/album-fotos.js
@@ -9,50 +9,60 @@ function photo(numPhoto) {
 		document.getElementById("img").src="img/fotos-snct/reufi01.jpg";
 		document.getElementById("description").innerHTML="Alunos André Henrique e João Paulo apresentado o site." ;
 		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
+		window.location.href = "#date";
 	}
 	else if ( numPhoto==2 ) {
 		document.getElementById("img").src="img/fotos-snct/reufi02.jpg";
 		document.getElementById("description").innerHTML="Aluno Manoel Eric falando a respeito do projeto." ;
 		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
+		window.location.href = "#date";
 	}
 	else if ( numPhoto==3 ) {
 		document.getElementById("img").src="img/fotos-snct/reufi03.jpg";
 		document.getElementById("description").innerHTML="Aluno João Paulo apresentando o projeto." ;
 		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
+		window.location.href = "#date";
 	}
 	else if ( numPhoto==4 ) {
 		document.getElementById("img").src="img/fotos-snct/reufi04.jpg";
 		document.getElementById("description").innerHTML="Aluno André Henrique falando a respeito do site." ;
 		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
+		window.location.href = "#date";
 	}
 	else if ( numPhoto==5 ) {
 		document.getElementById("img").src="img/fotos-snct/reufi05.jpg";
 		document.getElementById("description").innerHTML="Alunos André Henrique e João Paulo apresentando o projeto." ;
 		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
+		window.location.href = "#date";
 	}
 	else if ( numPhoto==6 ) {
 		document.getElementById("img").src="img/fotos-snct/reufi06.jpg";
 		document.getElementById("description").innerHTML="Alunos de Nutrição assistindo a palestra sobre aplicativos que já nos ajudam a manter uma vida saudável." ;
 		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
+		window.location.href = "#date";
 	}
 	else if ( numPhoto==7 ) {
 		document.getElementById("img").src="img/fotos-snct/reufi07.jpg";
 		document.getElementById("description").innerHTML="Alunos Manoel Eric e André Henrique apresentando o site." ;
 		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
+		window.location.href = "#date";
 	}
 	else if ( numPhoto==8 ) {
 		document.getElementById("img").src="img/fotos-snct/reufi08.jpg";
 		document.getElementById("description").innerHTML="Aluno João Paulo apresentando o site." ;
 		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
+		window.location.href = "#date";
 	}
 	else if ( numPhoto==9 ) {
 		document.getElementById("img").src="img/fotos-snct/reufi09.jpg";
 		document.getElementById("description").innerHTML="Aluna Stefany Brazil e aluno Tawan Rodrigues explicando as vantagens de se ingerir suco de manga." ;
 		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
+		window.location.href = "#date";
 	}
 	else if ( numPhoto==10 ) {
 		document.getElementById("img").src="img/fotos-snct/reufi10.jpg";
 		document.getElementById("description").innerHTML="Aluno Manoel Eric apresentando o site." ;
 		document.getElementById("viewClass").style.margin = "0px 0px 0px 0px";
+		window.location.href = "#date";
 	}
 }
